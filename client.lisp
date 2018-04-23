bd28e10a2eb1323dcd77feff1754d5c9926e63ce
sisto <sisto@sd.dk>
2018-04-23 09:04:06 +0200

    Working lisp client, copy of client.c

--- client.lisp
+++ client.lisp
@@ -1,7 +1,11 @@
-;; https://gist.github.com/shortsightedsid/a760e0d83a9557aaffcc
++;; https://gist.github.com/shortsightedsid/a760e0d83a9557aaffcc
 ;; https://bitbucket.org/elliottslaughter/blackthorn-engine-3d/src/94bc17efa497af519c1500797c89e90a477eded0/src/examples/usocket/usocket.lisp?at=default&fileviewer=file-view-default
 (ql:quickload :usocket)
 
 (defun socket-client ()
   (usocket:with-client-socket (client stream "127.0.0.1" 8080)
-    (format stream "Test")))
+                              (format stream "Test")
+                              (force-output stream)
+                              (format t (read-line stream nil))))
+
+
