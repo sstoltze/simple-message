c83c8b84541762aef26a39f9d28c38e9d42ecebb
Simon Stoltze <sstoltze@gmail.com>
2018-04-24 00:35:27 +0200

    Remove typo from file

--- client.lisp
+++ client.lisp
@@ -1,4 +1,4 @@
-+;; https://gist.github.com/shortsightedsid/a760e0d83a9557aaffcc
+;; https://gist.github.com/shortsightedsid/a760e0d83a9557aaffcc
 ;; https://bitbucket.org/elliottslaughter/blackthorn-engine-3d/src/94bc17efa497af519c1500797c89e90a477eded0/src/examples/usocket/usocket.lisp?at=default&fileviewer=file-view-default
 (ql:quickload :usocket)
 
