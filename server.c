b19d21b309eb1bad2dcdc0196bdc671667e51c3a
sisto <sisto@sd.dk>
2018-06-26 15:59:53 +0200

    Start work on server in C

---
+++ server.c
@@ -0,0 +1,88 @@
+
+// TODO: http://www.microhowto.info/howto/listen_for_and_accept_tcp_connections_in_c.html
+// Based on https://www.geeksforgeeks.org/socket-programming-cc/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+// TODO: Make configurable
+#define PORT 8080
+
+
+
+char *resize_buffer(char *buff, size_t *size) {
+  buff = realloc(buff, (*size)*2);
+  *size *= 2;
+  return buff;
+}
+
+int main() {
+  struct sockaddr_in address;
+  socklen_t addrlen = sizeof(address);
+  int serverfd;
+  int connectionfd;
+  int valread;
+  int opt = 1;
+  int server_backlog = 5; // Backlog of received messages.
+
+  char buffer[1024];
+  char *message = "Hello from C server.";
+
+  if ((serverfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("Error creating socket.");
+    exit(EXIT_FAILURE);
+  }
+
+  // Socket options. Optional, but useful
+  if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR,
+                 &opt, sizeof(opt))) {
+    perror("Error setting socket options.");
+    exit(EXIT_FAILURE);
+  }
+
+  // ?
+  address.sin_family = AF_INET;
+  // ?
+  address.sin_addr.s_addr = INADDR_ANY;
+  // ?
+  address.sin_port = htons(PORT);
+
+  // Attach socket
+  printf("Before bind.\n");
+  if (bind(serverfd, (struct sockaddr *)&address,
+           sizeof(address)) < 0) {
+    perror("Error binding socket.");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Before listen.\n");
+  if (listen(serverfd, server_backlog) < 0) {
+    perror("Error listening.");
+    exit(EXIT_FAILURE);
+  }
+  printf("Before accept.\n");
+  if ((connectionfd = accept(serverfd, (struct sockaddr *)&address,
+                            &addrlen)) < 0) {
+    perror("Error accepting.");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Connection established. Message received:\n");
+  valread = read(connectionfd, buffer, 1023);
+  if (valread != SO_ERROR) {
+    buffer[valread] = '\0';
+    printf("%s\n", buffer);
+    send(connectionfd, message, strlen(message), 0);
+    printf("Return message sent.\n");
+  } else {
+    printf("Error receiving message.\n");
+  }
+
+  close(connectionfd);
+  close(serverfd);
+  return 0;
+}
