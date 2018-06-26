a767af7e5f9e402929b3a78eddc527f639ae7fa0
sisto <sisto@sd.dk>
2018-06-26 19:10:54 +0200

    Accept multiple connections on C server. Cannot handle closed connection

--- server.c
+++ server.c
@@ -5,6 +5,8 @@
 #include <string.h>
 
 #include <sys/socket.h>
+// https://linux.die.net/man/2/select
+#include <sys/select.h>
 #include <netinet/in.h>
 #include <unistd.h>
 
@@ -22,6 +24,9 @@ char *server_read(int connectionfd, char *buffer, int *size) {
     buffer = resize_buffer(buffer, (*size)*2);
     valread += read(connectionfd, &buffer[*size], *size);
     (*size) *= 2;
+  }
+  if (valread == 0) {
+    return NULL;
   }
   if (valread != SO_ERROR) {
     buffer[valread] = '\0';
@@ -44,13 +49,15 @@ int main() {
   struct sockaddr_in address;
   socklen_t addrlen = sizeof(address);
   int serverfd;
-  int connectionfd;
   int valread;
   int opt = 1;
   int server_backlog = 5; // Backlog of received messages.
 
   int buffer_size = 1024;
   char *buffer = (char *)malloc(buffer_size*sizeof(char));
+  int number_of_connections = 0;
+  int max_connections = 1;
+  int *connectionfds = (int *)malloc(max_connections*sizeof(int));
   char *message = "Hello from C server.";
 
   if ((serverfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -67,9 +74,7 @@ int main() {
 
   // ?
   address.sin_family = AF_INET;
-  // ?
   address.sin_addr.s_addr = INADDR_ANY;
-  // ?
   address.sin_port = htons(PORT);
 
   // Attach socket
@@ -86,27 +91,78 @@ int main() {
     exit(EXIT_FAILURE);
   }
   printf("Before accept.\n");
-  if ((connectionfd = accept(serverfd, (struct sockaddr *)&address,
-                            &addrlen)) < 0) {
-    perror("Error accepting.");
-    exit(EXIT_FAILURE);
-  }
+  //setsockopt(serverfd, SOL_SOCKET, O_NONBLOCK, &opt, sizeof(opt));
 
-  printf("Connection established. Message received:\n");
-  buffer = server_read(connectionfd, buffer, &buffer_size);
-  /* valread = read(connectionfd, buffer, 1023);
-  if (valread != SO_ERROR) {
-  buffer[valread] = '\0'; */
-  if (buffer != NULL) {
-    printf("%s\n", buffer);
-    //send(connectionfd, message, strlen(message), 0);
-    server_send(&connectionfd, 1, message);
-    printf("Return message sent.\n");
-  } else {
-    printf("Error receiving message.\n");
-  }
+  // fd_set options
+  fd_set rfds;
+  struct timeval tv;
+  int retval;
 
-  close(connectionfd);
+  while(1) {
+    printf("Test.\n");
+    // New connections
+    FD_ZERO(&rfds);
+    FD_SET(serverfd, &rfds);
+    tv.tv_sec = 1; // Wait 1 sec
+    tv.tv_usec = 0; // And 0 ms
+    retval = select(FD_SETSIZE, &rfds, NULL, NULL, &tv);
+    if (retval == -1) {
+      printf("Error, server.\n");
+      break;
+    } else {
+      if (FD_ISSET(serverfd, &rfds)) {
+        printf("Server.\n");
+        if ((connectionfds[number_of_connections] = accept(serverfd, (struct sockaddr *)&address,
+                                                           &addrlen)) < 0) {
+          perror("Error accepting.");
+          exit(EXIT_FAILURE);
+        }
+        number_of_connections++;
+        if (number_of_connections == max_connections) {
+          max_connections *= 2;
+          connectionfds = (int *)realloc(connectionfds, max_connections*sizeof(int));
+        }
+        printf("Connection established.\n");
+      } else  {
+        printf("Not set on server.\n");
+      }
+    }
+
+    // Messages
+    FD_ZERO(&rfds);
+    for (int i=0;i<number_of_connections;i++) {
+      FD_SET(connectionfds[i], &rfds);
+    }
+    tv.tv_sec = 1; // Wait 1 sec
+    tv.tv_usec = 0; // And 0 ms
+    retval = select(FD_SETSIZE, &rfds, NULL, NULL, &tv);
+    if (retval == -1) {
+      printf("Error, message.\n");
+      break;
+    } else {
+      for (int i = 0;i<number_of_connections;i++) {
+        if (FD_ISSET(connectionfds[i], &rfds)) {
+          printf("Message received.\n");
+          buffer = server_read(connectionfds[i], buffer, &buffer_size);
+          if (buffer != NULL) {
+            printf("Sending message: %s\n", buffer);
+            server_send(connectionfds, number_of_connections, buffer);
+          } else {
+            printf("Error receiving message.\n");
+            close(connectionfds[i]);
+            // Make code to handle closing sockets...
+            break;
+          }
+        }
+      }
+    }
+  }
+  // Cleanup
+  for (int i=0; i<number_of_connections;i++) {
+    close(connectionfds[i]);
+  }
+  free(connectionfds);
   close(serverfd);
+  free(buffer);
   return 0;
 }
