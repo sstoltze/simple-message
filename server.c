faa88cf80e8aa46a30191e43c4fb5e69dd7c7960
sisto <sisto@sd.dk>
2018-06-26 16:33:05 +0200

    Start of server in C

--- server.c
+++ server.c
@@ -1,4 +1,3 @@
-
 // TODO: http://www.microhowto.info/howto/listen_for_and_accept_tcp_connections_in_c.html
 // Based on https://www.geeksforgeeks.org/socket-programming-cc/
 #include <stdio.h>
@@ -12,13 +11,34 @@
 // TODO: Make configurable
 #define PORT 8080
 
-
-
-char *resize_buffer(char *buff, size_t *size) {
-  buff = realloc(buff, (*size)*2);
-  *size *= 2;
+char *resize_buffer(char *buff, int new_size) {
+  buff = (char *)realloc(buff, new_size*sizeof(char));
   return buff;
 }
+
+char *server_read(int connectionfd, char *buffer, int *size) {
+  int valread = read(connectionfd, buffer, *size);
+  while (valread == *size && valread != SO_ERROR) {
+    buffer = resize_buffer(buffer, (*size)*2);
+    valread += read(connectionfd, &buffer[*size], *size);
+    (*size) *= 2;
+  }
+  if (valread != SO_ERROR) {
+    buffer[valread] = '\0';
+  }
+  else {
+    buffer = NULL;
+  }
+  return buffer;
+}
+
+void server_send(int *connectionfds, int num_connections, char *message) {
+  size_t mess_len = strlen(message);
+  for (int i=0;i < num_connections; i++) {
+    send(connectionfds[i], message, mess_len, 0);
+  }
+}
+
 
 int main() {
   struct sockaddr_in address;
@@ -29,7 +49,8 @@ int main() {
   int opt = 1;
   int server_backlog = 5; // Backlog of received messages.
 
-  char buffer[1024];
+  int buffer_size = 1024;
+  char *buffer = (char *)malloc(buffer_size*sizeof(char));
   char *message = "Hello from C server.";
 
   if ((serverfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -72,11 +93,14 @@ int main() {
   }
 
   printf("Connection established. Message received:\n");
-  valread = read(connectionfd, buffer, 1023);
+  buffer = server_read(connectionfd, buffer, &buffer_size);
+  /* valread = read(connectionfd, buffer, 1023);
   if (valread != SO_ERROR) {
-    buffer[valread] = '\0';
+  buffer[valread] = '\0'; */
+  if (buffer != NULL) {
     printf("%s\n", buffer);
-    send(connectionfd, message, strlen(message), 0);
+    //send(connectionfd, message, strlen(message), 0);
+    server_send(&connectionfd, 1, message);
     printf("Return message sent.\n");
   } else {
     printf("Error receiving message.\n");
